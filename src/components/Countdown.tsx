e1381b1f65dce5e0c3e7256bc3b0b806c3625552
Audrow Nash <audrow@hey.com>
2022-09-24 22:17:32 -0500

    Add confetti + countdown timer
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useEffect, useState } from 'react';
+
+type TimeRemaining = {
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+const CountdownTimer = ({targetDate, showSeconds=true}: { targetDate: Date, showSeconds?: boolean }) => {
+
+  const [timeRemaining, setTimeRemaining] = useState<TimeRemaining | undefined>()
+  const [loading, setLoading] = useState(true)
+  const [finished, setFinished] = useState(false)
+
+  useEffect(() => {
+    const targetTime = targetDate.getTime()
+    setInterval(() => {
+      const countDown = targetTime - new Date().getTime()
+      const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+      setTimeRemaining({
+        days, hours, minutes, seconds
+      })
+      setFinished(days + hours + minutes + seconds < 0)
+      if (loading) {
+        setLoading(false)
+      }
+    }, 1000)
+  })
+  if (loading) {
+    return (
+      <div>Loading timer</div>
+    )
+  } else if (finished) {
+    return (
+      <div>They should be married by now!</div>
+    )
+  } else {
+    return (
+      <div className='flex flex-row gap-6'>
+        <TimeCard value={timeRemaining!.days} units={'Days'} />
+        <TimeCard value={timeRemaining!.hours} units={'Hours'} />
+        <TimeCard value={timeRemaining!.minutes} units={'Minutes'} />
+        {showSeconds && <TimeCard value={timeRemaining!.seconds} units={'Seconds'} />
+        }
+      </div>
+    )
+  }
+};
+
+function TimeCard({value, units}: {value: number, units: string}) {
+  return (
+      <div className='flex flex-col gap-2 items-center'>
+        <div className='bg-white p-4 border-yellow-400 border-4 rounded-xl'>
+          {value}
+        </div>
+        <div>
+          {units}
+        </div>
+      </div>
+  )
+}
+
+export default CountdownTimer;
--- src/pages/new2.tsx
+++ src/pages/new2.tsx
@@ -1,5 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
-import { HomeIcon } from '@heroicons/react/24/outline'
+import { HomeIcon } from '@heroicons/react/24/solid'
+import confetti from 'canvas-confetti'
+import mainImage from '../../public/imgs/broken-spoke.png'
+import Image from 'next/image'
+import * as config from "../../config";
+import CountdownTimer from "../components/Countdown";
 
 const selectedClasses = "inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
 const notSelectedClasses = "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
@@ -23,28 +28,56 @@ const scrollTo = (ele: HTMLElement) => {
   });
 };
 
+function confettiFireworks() {
+  const duration = 5 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults: confetti.Options = {
+    startVelocity: 30, spread: 360, ticks: 60, zIndex: 50,
+    particleCount: 200,
+    colors: ['#c61626', '#feda01', '#e15b54', '#f3f8fb'],
+  };
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval: NodeJS.Timer = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
+}
+
 function App() {
   const [visibleSection, setVisibleSection] = useState<string | undefined>();
 
-  const topRef = useRef(null);
-  const headerRef = useRef(null);
-  const leadershipRef = useRef(null);
-  const providerRef = useRef(null);
-  const operationsRef = useRef(null);
+  const navRef = useRef(null);
+  const homeRef = useRef(null);
+  const locationRef = useRef(null);
+  const scheduleRef = useRef(null);
+  const detailsRef = useRef(null);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const sectionRefs = [
-    { section: "Leadership", ref: leadershipRef },
-    { section: "Providers", ref: providerRef },
-    { section: "Operations", ref: operationsRef },
+    { section: "Home", ref: homeRef },
+    { section: "Location", ref: locationRef },
+    { section: "Schedule", ref: scheduleRef },
+    { section: "Details", ref: detailsRef },
   ];
 
   useEffect(() => {
     const handleScroll = () => {
-      if (!headerRef.current) {
+      if (!navRef.current) {
         return
       }
-      const { height: headerHeight } = getDimensions(headerRef.current);
+      const { height: headerHeight } = getDimensions(navRef.current);
       const scrollPosition = window.scrollY + headerHeight;
 
       const selected = sectionRefs.find(({ ref }) => {
@@ -69,67 +102,93 @@ function App() {
     };
   }, [sectionRefs, visibleSection]);
 
+
   return (
-    <div className="App">
-      <div ref={topRef} className="h-96">
-        <h2 className="text-4xl font-bold text-center">Scroll to see sticky header</h2>
-      </div>
-
-      <div className="content">
-        <div className="sticky top-0">
-          <div className="flex justify-center bg-white" ref={headerRef}>
-            <button
-              type="button"
-              className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              onClick={() => {
-                if (topRef.current) {
-                  scrollTo(topRef.current);
-                }
-              }}
-            >
-              <HomeIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
-            <button
-              type="button"
-              className={`${visibleSection === "Leadership" ? selectedClasses : notSelectedClasses}`}
-              onClick={() => {
-                if (leadershipRef.current) {
-                  scrollTo(leadershipRef.current);
-                }
-              }}
-            >
-              Leadership
-            </button>
-            <button
-              type="button"
-              className={`${visibleSection === "Providers" ? selectedClasses : notSelectedClasses}`}
-              onClick={() => {
-                if (providerRef.current) {
-                  scrollTo(providerRef.current);
-                }
-              }}
-            >
-              Providers
-            </button>
-            <button
-              type="button"
-              className={`${visibleSection === "Operations" ? selectedClasses : notSelectedClasses}`}
-              onClick={() => {
-                if (operationsRef.current) {
-                  scrollTo(operationsRef.current);
-                }
-              }}
-            >
-              Operations
-            </button>
-          </div>
+    <div>
+      {/* Navbar */}
+      <nav className="sticky bg-white top-0 flex justify-center py-4 z-20">
+        <div className="flex gap-3" ref={navRef}>
+          <button
+            type="button"
+            className={`${visibleSection === "Home" ? selectedClasses : notSelectedClasses}`}
+            onClick={() => {
+              if (homeRef.current) {
+                scrollTo(homeRef.current);
+              }
+            }}
+          >
+            <HomeIcon className="h-5 w-5 mr-1 text-red-600" aria-hidden="true" /> Home
+          </button>
+          <button
+            type="button"
+            className={`${visibleSection === "Location" ? selectedClasses : notSelectedClasses}`}
+            onClick={() => {
+              if (locationRef.current) {
+                scrollTo(locationRef.current);
+              }
+            }}
+          >
+            Location
+          </button>
+          <button
+            type="button"
+            className={`${visibleSection === "Schedule" ? selectedClasses : notSelectedClasses}`}
+            onClick={() => {
+              if (scheduleRef.current) {
+                scrollTo(scheduleRef.current);
+              }
+            }}
+          >
+            Schedule
+          </button>
+          <button
+            type="button"
+            className={`${visibleSection === "Details" ? selectedClasses : notSelectedClasses}`}
+            onClick={() => {
+              if (detailsRef.current) {
+                scrollTo(detailsRef.current);
+              }
+            }}
+          >
+            Details
+          </button>
         </div>
-        <div className="h-40 bg-slate-300" id="Leadership" ref={leadershipRef} />
-        <div className="h-60 bg-blue-300" id="Providers" ref={providerRef} />
-        <div className="h-80 bg-red-300" id="Operations" ref={operationsRef} />
-        <div className="h-screen" />
-      </div>
+        <div className="ml-6">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={confettiFireworks}
+          >
+            RSVP
+          </button>
+        </div>
+      </nav>
+      {/* Home */}
+      <div className="bg-red-200 flex flex-col py-10 items-center" id="Home" ref={homeRef}>
+        <h1 className="text-center text-5xl font-bold text-red-600 pt-10">We{"'"}re Getting Married!</h1>
+        <div className="w-1/2 my-10 shadow rounded">
+          <Image priority src={mainImage} alt="Audrow and Michelley" layout="responsive" width={mainImage.width} height={mainImage.height} />
+        </div>
+        <button
+          className="text-4xl font-bold text-red-600 bg-yellow-300 rounded-xl shadow border-4 border-yellow-500 p-4"
+          onClick={confettiFireworks}
+        >11/11/22 in San Antonio, Tx</button>
 
+        <div className="my-6">
+          <CountdownTimer targetDate={config.weddingDetails.ceremony.start} showSeconds={true} />
+        </div>
+
+      </div>
+      {/* Location */}
+      <div className="h-60 bg-blue-300" id="Location" ref={locationRef}>
+      </div>
+      <div className="h-80 bg-red-300" id="Schedule" ref={scheduleRef}>
+      </div>
+      <div className="h-80 bg-green-300" id="Details" ref={detailsRef}>
+      </div>
+      <div className="h-screen text-center flex flex-col justify-center">
+        Website made with love by Audrow and Michelley
+      </div>
     </div>
   );
 }
