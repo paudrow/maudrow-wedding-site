2d4c9f31b75d7b17f16d254d21597bd10d58def2
Audrow Nash <audrow@hey.com>
2022-09-24 22:26:49 -0500

    Tune confetti
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/pages/new2.tsx
+++ src/pages/new2.tsx
@@ -32,8 +32,7 @@ function confettiFireworks() {
   const duration = 5 * 1000;
   const animationEnd = Date.now() + duration;
   const defaults: confetti.Options = {
-    startVelocity: 30, spread: 360, ticks: 60, zIndex: 50,
-    particleCount: 200,
+    startVelocity: 20, spread: 360, ticks: 60, zIndex: 50,
     colors: ['#c61626', '#feda01', '#e15b54', '#f3f8fb'],
   };
 
@@ -48,7 +47,7 @@ function confettiFireworks() {
       return clearInterval(interval);
     }
 
-    const particleCount = 50 * (timeLeft / duration);
+    const particleCount = 300 * (timeLeft / duration);
     // since particles fall down, start a bit higher than random
     confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
     confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
