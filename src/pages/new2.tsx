305d14aab9c67533290e1f47dea966ce68c4da9d
mishmishmish <81111968+mishmishmish@users.noreply.github.com>
2022-09-26 17:52:54 -0500

    Mish-add-graphics (#2)
    * added lanterns + animation
    
    * attempt at schedule

--- src/pages/new2.tsx
+++ src/pages/new2.tsx
@@ -11,7 +11,7 @@ import CountdownTimer from "../components/Countdown";
 
 const desktopSelectedClasses = "inline-flex items-center border-b-2 border-big-red px-1 pt-1 text-sm font-medium text-big-red"
 const desktopNotSelectedClasses = "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-fire-opal hover:text-gray-800"
-const mobileSelectedClasses = "block border-l-4 border-indigo-500 bg-big-red py-2 pl-3 pr-4 text-base font-medium text-big-red"
+const mobileSelectedClasses = "block border-l-4 border-big-red bg-pink py-2 pl-3 pr-4 text-base font-medium text-big-red"
 const mobileNotSelectedClasses = "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-fire-opal hover:bg-gray-50 hover:text-gray-800"
 
 const getDimensions = (ele: HTMLElement) => {
@@ -110,13 +110,13 @@ function App() {
   function Navbar() {
     return (
       <Disclosure as="nav" className="bg-alice-blue shadow sticky top-0 z-20">
-      {({ open }) => (
+        {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
               <div className="relative flex h-16 justify-center">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-600 hover:bg-fire-opal hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-big-red">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-big-red hover:bg-pink hover:text-fire-opal focus:outline-none focus:ring-2 focus:ring-inset focus:ring-big-red">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -230,13 +230,23 @@ function App() {
       {/* <Navbar /> */}
       <Navbar />
       {/* Home */}
-      <div className="bg-pink flex flex-col py-10 items-center" id="Home" ref={homeRef}>
-        <h1 className="text-center font-bangers text-5xl font-bold text-big-red sm:pt-10">We{"'"}re Getting Married!</h1>
+      <div className="bg-pink flex flex-col items-center" id="Home" ref={homeRef}>
+        {/*lanterns*/}
+        <div className="flex items-start lg:gap-20 gap-5 md:gap-10">
+          <object data="/svg/lantern1.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top hidden sm:block"></object>
+          <object data="/svg/lantern2.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top"></object>
+          <object data="/svg/lantern3.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top" ></object>
+          <object data="/svg/lantern4.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top"></object>
+          <object data="/svg/lantern5.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top"></object>
+          <object data="/svg/lantern6.svg" className="h-20 flex-shrink md:h-full animate-swing origin-top"></object>
+
+        </div>
+        <h1 className="text-center font-bangers text-3xl md:text-6xl font-bold text-big-red mt-5">We{"'"}re Getting Married!</h1>
         <div className="w-full lg:w-2/3 sm:w-3/4 my-10 shadow rounded">
           <Image priority src={mainImage} alt="Audrow and Michelley" layout="responsive" width={mainImage.width} height={mainImage.height} />
         </div>
         <button
-          className="inline-flex items-center text-4xl font-bangers text-big-red bg-sizzling-sunrise hover:bg-amber-400 rounded-xl shadow border-4 border-harvest-gold py-4 px-10"
+          className="inline-flex items-center text-3xl md:text-5xl font-bangers text-big-red bg-sizzling-sunrise hover:bg-amber-400 rounded-xl shadow border-4 border-harvest-gold py-4 px-4 sm:px-8 md:px-14 lg:px-18"
           onClick={confettiFireworks}
         >
           11/11/22 &nbsp;in &nbsp;San Antonio, Tx</button>
@@ -252,11 +262,11 @@ function App() {
               <Image src={parkImage} alt="Audrow and Michelley in the park" layout="responsive" width={parkImage.width} height={parkImage.height} />
             </div>
           </div>
-          <div className="col-span-1 text-center my-10">
+          <div className="col-span-1 text-center my-10 mx-4 md:mx-2 lg:mx-10">
             <div className="flex justify-center">
-              <MapPinIcon className="block h-12 w-12" aria-hidden="true" />
+              <object data="/svg/venue.svg"></object>
             </div>
-            <h2 className="text-xl">Venue</h2>
+            <h2 className="text-xl font-bold">VENUE</h2>
             <div>
               <p>
                 Phil Hardberger park is a 300+ acre nature park near us, it{'’'}s where we go for our weekly Sunday hike and where we got engaged!
@@ -267,18 +277,18 @@ function App() {
               </p>
             </div>
             <button
-              className="mt-6 px-4 py-2 bg-white border-red-600 border-4 rounded-xl text-red-600"
+              className="mt-6 px-4 py-2 bg-white border-red-600 border-2 rounded-xl text-red-600"
             >
               <a href="https://goo.gl/maps/xYZbHhp1sPpjzv8t6">
                 See on map
               </a>
             </button>
           </div>
-          <div className="col-span-1 text-center my-10">
+          <div className="col-span-1 text-center my-10 mx-4 md:mx-2 lg:mx-10">
             <div className="flex justify-center">
-              <BriefcaseIcon className="block h-12 w-12" aria-hidden="true" />
+              <object data="/svg/hotel.svg"></object>
             </div>
-            <h2 className="text-xl">Hotel</h2>
+            <h2 className="text-xl font-bold">HOTEL</h2>
             <div>
               <p>
                 There are plenty of hotel options near the airport and downtown, feel free to choose whatever works for you.
@@ -289,7 +299,7 @@ function App() {
               </p>
             </div>
             <button
-              className="mt-6 px-4 py-2 bg-white border-red-600 border-4 rounded-xl text-red-600"
+              className="mt-6 px-4 py-2 bg-white border-red-600 border-2 rounded-xl text-red-600"
             >
               <a href="https://goo.gl/maps/NAMGszvCseSQZ3Qi7">
                 See on map
@@ -357,9 +367,44 @@ function App() {
           </p>
       </div>
         </div> */}
-      <div className="h-80 bg-alice-blue" id="Schedule" ref={scheduleRef}>
+      <div className="bg-alice-blue" id="Schedule" ref={scheduleRef}>
+        <div className="grid grid-cols-2 gap-10 text-center py-8 md:px-10">
+          <div className="font-bold">
+            THURSDAY
+          </div>
+          <div className="font-bold">
+            FRIDAY
+          </div>
+          <div> {/*thursday*/}
+            6:00 pm
+            <br/>
+            Welcome dinner at our favourite Texas BBQ, The Smoke Shack
+            <div className="text-big-red">3714 Broadway</div>
+          </div>
+          <div> {/*friday*/}
+            4:00 pm
+            <br/>
+            Come to the park for photos & enjoy the views
+          </div>
+          <div>{/*blank thursday*/}</div>
+          <div> {/*friday*/}
+            4:30 pm
+            <br/>
+            The wedding ceremony! & more photos during Golden Hour
+          </div>
+          <div>{/*blank thursday*/}</div>
+          <div> {/*friday*/}
+            6:00 pm
+            <br/>
+            Hot pot dinner at Sichuan Garden
+            <div className="text-big-red">2347 NW Military Hwy</div>
+          </div>
+        </div>
       </div>
       <div className="h-80 bg-pink" id="Details" ref={detailsRef}>
+        <div className="font-bangers text-big-red text-center p-4 text-3xl md:text-5xl">
+          We can{"'"}t wait to see you!
+        </div>
       </div>
       <div className="h-screen text-center flex flex-col justify-center">
         Website made with love by Audrow and Michelley
