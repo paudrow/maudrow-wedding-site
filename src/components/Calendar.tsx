7e697841919e41685386bf818d2fe91b66e90d8b
Audrow Nash <audrow@hey.com>
2022-09-28 17:27:08 -0500

    Fix SVGs to work with links
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -7,21 +7,21 @@ export default function Calendar({ calendarOptions }: { calendarOptions: Calenda
       <p>Add to calendar</p>
       <div className="flex flex-row justify-evenly gap-3 pt-3">
         <button onClick={() => new ICalendar(calendarOptions).download()}>
-          <object data="/svg/apple.svg"/>
+          <object className="pointer-events-none" data="/svg/apple.svg"/>
         </button>
         <button>
           <a target="_blank" href={new GoogleCalendar(calendarOptions).render()} rel="noopener noreferrer">
-          <object data="/svg/google.svg"/>
+          <object className="pointer-events-none" data="/svg/google.svg"/>
           </a>
         </button>
         <button>
           <a target="_blank" href={new YahooCalendar(calendarOptions).render()} rel="noopener noreferrer">
-          <object data="/svg/yahoo.svg"/>
+          <object className="pointer-events-none" data="/svg/yahoo.svg"/>
           </a>
         </button>
         <button>
           <a target="_blank" href={new OutlookCalendar(calendarOptions).render()} rel="noopener noreferrer">
-          <object data="/svg/ms.svg"/>
+            <object className="pointer-events-none" data="/svg/ms.svg"/>
           </a>
         </button>
       </div>
